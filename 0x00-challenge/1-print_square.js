8a0364a16a679161f074d62c657fe122fa5f2d4c
omnia abd elaleem mohamed <120645154+OMNIA93@users.noreply.github.com>
2023-10-29 01:17:15 +0200

    Update 1-print_square.js

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -13,11 +13,12 @@ if (process.argv.length <= 2) {
     process.exit(1);
 }
 
-size = parseInt(process.argv[2], 10)
+let size = parseInt(process.argv[2], 10); 
 
 for (let i = 0; i < size; i++) {
+    let line = "";
     for (let j = 0; j < size; j++) {
-        process.stdout.write("#");
+        line += "#";
     }
-    process.stdout.write("\n");
+    console.log(line);
 }
