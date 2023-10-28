456e3c182e60b7cd9a17a886d35768910a4fe962
omnia abd elaleem mohamed <120645154+OMNIA93@users.noreply.github.com>
2023-10-29 01:50:10 +0200

    Update 1-print_square.js

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -6,19 +6,19 @@
     of the program.
 */
 
+
 if (process.argv.length <= 2) {
     process.stderr.write("Missing argument\n");
     process.stderr.write("Usage: ./1-print_square.js <size>\n");
     process.stderr.write("Example: ./1-print_square.js 8\n");
-    process.exit(1);
+    process.exit(1)
 }
 
-let size = parseInt(process.argv[2], 10); 
+size = parseInt(process.argv[2], 10)
 
-for (let i = 0; i < size; i++) {
-    let line = "";
-    for (let j = 0; j < size; j++) {
-        line += "#";
+for (let i = 0 ; i < size ; i ++) {
+    for (let j = 0 ; j < size ; j ++) {
+        process.stdout.write("#");
     }
-    console.log(line);
+    process.stdout.write("\n");
 }
